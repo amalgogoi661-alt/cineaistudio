2edc6fe94ded8ddb87edb4e174a8038b1274621b
amalgogoi661-alt <amalgogoi661@gmail.com>
2026-06-29 13:18:19 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,1 +1,17 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
 
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "cineai-studio-c63ca.firebaseapp.com",
+  projectId: "cineai-studio-c63ca",
+  storageBucket: "cineai-studio-c63ca.firebasestorage.app",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
